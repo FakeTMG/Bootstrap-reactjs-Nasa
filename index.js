784da7384728f0ebf157297a2c4c8044f76d0f13
unknown <the.tunwebdev@gmail.com>
2021-07-20 21:00:48 +0200

    adding new things in backend

--- index.js
+++ index.js
@@ -6,30 +6,44 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json()); //req.body
 
-app.post('/login',async(req,res)=>{
+app.post('/signup',async(req,res)=>{
     try{
-        const {email,password,msg} = req.body
-        const newUser = await pool.query(
-            "INSERT INTO loginandsign (email,password,description) VALUES($1,$2,$3) RETURNING *",
-            [email,password,msg]
-          );
-        res.json(newUser.rows[0])  
+        const {sign_email,sign_password,sign_msg} = req.body
+        if(sign_email === '' || sign_password === ""){
+            res.status(400).json('email and password are required')
+        }
+        const checkForEmail = await pool.query('SELECT * FROM loginandsign WHERE  email=$1',[sign_email])
+        if(checkForEmail.rows.length > 0){
+            res.status(400).json('wrong credentials')
+        }else{
+            const newUser = await pool.query(
+                "INSERT INTO loginandsign (email,password,description) VALUES($1,$2,$3) RETURNING *",
+                [sign_email,sign_password,sign_msg]
+              );
+            res.json(newUser.rows[0])  
+
+        }
+       
+        
 
     }catch(e){
         console.log(e)
     }
 
 })
-app.get('/login',async(req,res)=>{
+app.post('/login',async(req,res)=>{
     try{
-        const {email,password} = req.body
+        const {login_email,login_password} = req.body
+        if(login_email === '' || login_password === ""){
+            res.status(400).json('email and password are required')
+        }
         
-        const checkUser= await pool.query('SELECT * FROM loginandsign WHERE  email=$1 AND password=$2 ',[email,password])
+        const checkUser= await pool.query('SELECT * FROM loginandsign WHERE  email=$1 AND password=$2 ',[login_email,login_password])
        
-        if(checkUser.rowCount !== 0){
+        if(checkUser.rows.length > 0){
             res.json(checkUser.rows[0])
         }else{
-            res.json('wrong credentials')
+            res.status(400).json('wrong credentials')
         }
 
     }
