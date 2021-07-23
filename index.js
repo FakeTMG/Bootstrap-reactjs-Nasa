50ab5f04e32943b3b0b873221c50525a635c62c7
unknown <the.tunwebdev@gmail.com>
2021-07-23 15:48:02 +0200

    backend algo for change pass

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const pool = require("./db");
 
 app.use(cors());
-app.use(express.json()); //req.body
+app.use(express.json()); 
 
 app.post('/signup',async(req,res)=>{
     try{
@@ -55,6 +55,63 @@ app.post('/login',async(req,res)=>{
     
    
 })
+app.post('/',async(req,res)=>{
+    try{
+        const {title,url,explanation,date,user_email} = req.body
+        const addTofav = await pool.query(
+            "INSERT INTO user_posts (post_title,post_image,post_description,post_footer,email) VALUES($1,$2,$3,$4,$5) RETURNING *",
+            [title,url,explanation,date,user_email]
+          );
+        res.json(addTofav.rows[0])  
+
+    }catch(err){
+        console.log(err.message)
+
+    }
+})
+app.post('/fav',async(req,res)=>{
+    try{
+        const {user_email} =  req.body
+        const getFav =  await pool.query('SELECT post_id,post_title,post_image,post_description,post_footer FROM user_posts WHERE  email=$1 ',[user_email])
+        res.json(getFav.rows)
+
+
+    }catch(err){
+        consle.log(err.message)
+
+    }
+})
+app.delete("/fav/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await pool.query("DELETE FROM user_posts WHERE post_id = $1", [
+        id
+      ]);
+      console.log('delete ')
+      res.status(200).json('wrong credentials')
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+app.post('/pass_change',async(req,res)=>{
+    try{
+        const {currentPass,user_email,new_pass} =  req.body
+        const checkCurrentPassword =  await pool.query('SELECT password FROM loginandsign WHERE  email=$1 ',[user_email])
+        if(checkCurrentPassword.rows[0].password === currentPass){
+            
+            
+            updatepassword =  await pool.query('update loginandsign set password = $1 where email=$2 returning * ;',[new_pass,user_email])
+            res.json(updatepassword.rows[0].password)
+            console.log('cbon')
+            
+        }else{
+            res.status(200).json('current password is incorrect')
+        }
+
+    }catch(err){
+
+    }
+})  
 app.listen(5000, () => {
     console.log("server has started on port 5000");
   });
